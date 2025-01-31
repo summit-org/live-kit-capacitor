7a41df4c4a336d16293b09c92c7413422c232460
Tripp Roberts <tripp@tripp-roberts.com>
2025-01-31 11:17:27 -0500

    tweak android

--- android/src/main/java/com/livekit/capacitor/plugin/LiveKitPlugin.kt
+++ android/src/main/java/com/livekit/capacitor/plugin/LiveKitPlugin.kt
@@ -7,42 +7,52 @@ import com.getcapacitor.annotation.CapacitorPlugin
 import io.livekit.android.LiveKit.create
 import io.livekit.android.LiveKitOverrides
 import io.livekit.android.RoomOptions
+import io.livekit.android.room.Room
 import io.livekit.android.room.track.RemoteTrackPublication
 import kotlinx.coroutines.runBlocking
 
 @CapacitorPlugin(name = "LiveKit")
 class LiveKitPlugin : Plugin() {
-    private val room = create(activity.applicationContext, RoomOptions(), LiveKitOverrides())
+    private var room: Room? = null;
+
     @PluginMethod
     fun connect(call: PluginCall) {
+        if (room == null) {
+            room = create(activity.applicationContext)
+        }
         val url = call.getString("url")
         val token = call.getString("token")
 
         runBlocking {
-            room.connect(url!!, token!!)
-            room.localParticipant.setCameraEnabled(false)
-            room.localParticipant.setMicrophoneEnabled(true)
+            room!!.connect(url!!, token!!)
+            room!!.localParticipant.setCameraEnabled(false)
+            room!!.localParticipant.setMicrophoneEnabled(true)
+            room!!.localParticipant.setScreenShareEnabled(false)
         }
         call.resolve()
     }
 
     @PluginMethod
     fun disconnect(call: PluginCall) {
-        room.disconnect()
+        if (room != null) {
+            room!!.disconnect()
+        }
         call.resolve()
     }
 
     @PluginMethod
     fun setMuted(call: PluginCall) {
         val muted = call.getBoolean("muted") ?: false;
-        runBlocking {
-                room.localParticipant.setMicrophoneEnabled(muted)
-                    for (participant in room.remoteParticipants.values) {
-                        for (publication in participant.audioTrackPublications) {
-                            (publication.first as RemoteTrackPublication).setSubscribed(!muted)
-                        }
+        if (room != null) {
+            runBlocking {
+                room!!.localParticipant.setMicrophoneEnabled(muted)
+                for (participant in room!!.remoteParticipants.values) {
+                    for (publication in participant.audioTrackPublications) {
+                        (publication.first as RemoteTrackPublication).setSubscribed(!muted)
                     }
+                }
             }
+        }
         call.resolve()
     }
 }
